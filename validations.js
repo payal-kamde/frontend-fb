c776fe3889d5e8d68c03208f20be358834066549
dubeynainan146 <dubeynainan146@gmail.com>
2022-11-14 14:34:53 +0530

    changes commited

--- validations.js
+++ validations.js
@@ -1,48 +1,36 @@
-function f1(){
-    var emailVal=document.form1.Email.value;
-    if(emailVal.indexOf('.')<=0){
-        document.getElementById("em").innerHTML="INVALID . POSITION";
-        return false;
-    }
-    if((emailVal.charAt(emailVal.length-4)!='.') && (emailVal.length-3)!='.'){
-        document.getElementById("em").innerHTML="INVALID";
-        return false;
-    }
-}
-function validationfunc(){
-    var a= document.form1.Email.value;
-    if(a.indexOf('@')<=0){
-        alert("**Invalid position of @")
-        return false;
-        }
-    var name=document.form1.Username.value;
-    var password=document.form1.Password.value;
-    if(name==null||name==""){
+console.log("Handling onsubmit events");
 
-        alert("  Username cannot be empty ")
-        return false;
+const form=document.querySelector('form')
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+
+    console.log("form submitted", event);
+
+    const username = document.querySelector('#username')
+    const email =document.querySelector('#email')
+    const password=document.querySelector('#password1')
+    const confirmPassword=document.querySelector('#password2')
+
+    const usernameValue=username.value
+    const emailValue=email.value
+    const passwordValue=password.value
+    const confirmPasswordValue=confirmPassword.value
+
+    if(!usernameValue){
+        alert("Username cannot be empty")
+        return
     }
-     
-    if(password.length<=7){
-        document.getElementById("ps").innerHTML("Password Must Be Of 8 cahracters")
-    }
-    console.log("dfhj")
-}    
-function validatefunction(){
-    
-     var e1=document.f1.email.value;
-     var pass1=document.f1.password.value;
-     if(e1==null||e1==""){
+    if(!emailValue){
         alert("Email cannot be empty")
-        return false;
-     }
-    if (pass1.length<8){
-        alert("Password cannot be less than 8 characters")
-        return false;
-     }
-    if(e1.indexOf("@")<=0){
-        alert("@ need to be included at correct position")
-        return false;
-     }
-   
-}
+        return
+    }
+    if(!passwordValue){
+        alert("Password cannot be empty")
+        return
+    }
+    if(!confirmPasswordValue){
+        alert("confirm Password cannot be empty")
+    }
+
+})
