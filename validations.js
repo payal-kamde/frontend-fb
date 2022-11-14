49d49a2b82b32e45c38e3e866a76c2956acb0989
dubeynainan146 <dubeynainan146@gmail.com>
2022-11-15 00:45:39 +0530

    Validations changes updated

--- validations.js
+++ validations.js
@@ -1,74 +1,92 @@
-console.log("Handling onsubmit events");
-
-const form=document.querySelector('#fbForm');
-console.log(form, '------')
-
+const form = document.querySelector('#fbForm');
 form.addEventListener('submit', (event) =>{
     event.preventDefault()
 
     console.log("form submitted", event);
-
-    const email =document.querySelector('#email')
-    const password=document.querySelector('#password1')
-    const confirmPassword=document.querySelector('#password2')
-
-    const username = document.querySelector('#username')
-    const usernameValue=username.value
-    const emailValue=email.value
-    const passwordValue=password.value
-    const confirmPasswordValue=confirmPassword.value
-
-    const usernameRegex=new RegExp(" ^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$")
-    const emailRegex=new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-    const passwordRegex=new RegExp("(?=.*\d.*)(?=.*[a-zA-Z].*)(?=.*[!#\$%&\?].*).{8,}")
-
-    function showErrorMessage(input, message, elementId){
-        const errorMessage = input.querySelector(elementId)
-        console.log(errorMessage, '-------', elementId)
-        errorMessage.innerText=message;
-    }
-
-    function usernameValidation(){
-        if(!usernameValue){
-            return showErrorMessage(username, '**Username should not be empty', '#usernamediv')
-        }
-        else if(!usernameRegex.test(usernameValue)){
-            return showErrorMessage(username, '**enter valid username', '#usernamediv');
-        }
-        return
-    }
-
-    function emailValidation(){
-        if(!emailValue){
-            return showErrorMessage(email, '**Email cannot be empty', '#emaildiv')
-        }
-        else if(!emailRegex.test(emailValue)){
-            return showErrorMessage(email, '**enter valid email id', '#emaildiv');
-        }
-        return
-    }
-
-    function passwordValidation(){
-        if(!passwordValue){
-            return showErrorMessage(password, '**Password must be entered', '#passworddiv')
-        }
-        else if(!passwordRegex.test(passwordValue)){
-            return showErrorMessage(password, '**Emter valid password', '#passworddiv')
-        }
-        return
-    }
-
-    function confirmPasswordValidation(){
-        if(!confirmPasswordValue){
-            return showErrorMessage(password, '**Please Re-enter Password','#confirmpassworddiv')
-        }
-        else if(passwordValue != confirmPasswordValue){
-            return showErrorMessage(confirmPassword,'password does not match', '#confirmpassworddiv');
-        }
-    }
     
     usernameValidation();
     emailValidation();
     passwordValidation();
     confirmPasswordValidation();
-})
+});
+
+function showErrorMessage(message, elementId){
+    const errorMessage = document.querySelector(elementId)
+    errorMessage.innerText=message;
+}
+
+function showSuccessMessage(successMsg, elementId){
+    const noErrorMessage=document.querySelector(elementId)
+    noErrorMessage.innerText=successMsg;
+}
+
+function usernameValidation(){
+
+    const username = document.querySelector('#username')
+    const usernameValue = username.value;
+
+    const usernameRegex=new RegExp("[A-Za-z][A-Za-z0-9_]{7,29}$")
+
+    if (!usernameValue){
+        showErrorMessage('**Username should not be empty', '#user-error-block');
+    } else if (!usernameRegex.test(usernameValue)){
+        showErrorMessage('**enter valid username', '#user-error-block');
+    }
+    else{
+        showSuccessMessage('Valid Username', '#user-error-block');
+    }
+}
+
+function emailValidation(){
+
+    const email =document.querySelector('#email')
+    const emailValue=email.value
+
+    const emailRegex=new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+    if(!emailValue){
+        showErrorMessage('**Email cannot be empty', '#email-error-block');
+    }
+    else if(!emailRegex.test(emailValue)){
+        showErrorMessage(e, '**enter valid email id', '#email-error-block');
+    }
+    else{
+        showSuccessMessage('Valid Email', '#email-error-block');
+    }
+
+}
+
+function passwordValidation(){
+
+    const password=document.querySelector('#password')
+    const passwordValue=password.value
+    
+    const passwordRegex=new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")
+
+    if(!passwordValue){
+        showErrorMessage('**Password must be entered properly', '#password-error-block');
+    }
+    else if(!passwordRegex.test(passwordValue)){
+        showErrorMessage('**Enter valid password', '#password-error-block');
+    }
+    else{
+        showSuccessMessage('Valid Password', '#password-error-block');
+    }
+}
+
+function confirmPasswordValidation(){
+    
+    const passwordValidate=password.value
+    const confirmPassword=document.querySelector('#confirm-password')
+    const confirmPasswordValue=confirmPassword.value
+
+    if(!confirmPasswordValue){
+        showErrorMessage('**Please Re-enter Password','#confirm-password-error-block');
+    }
+    else if(passwordValidate != confirmPasswordValue){
+        showErrorMessage('password does not match', '#confirm-password-error-block');
+    }
+    else{
+        showSuccessMessage('Password matched', '#confirm-password-error-block');
+    }
+}
